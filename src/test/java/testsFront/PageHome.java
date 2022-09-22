0adfb6ba61a32818cc3020fbc327707e975771bc
Ale <aleymblira@gmail.com>
2022-09-22 19:50:51 -0300

    testes senai

--- src/test/java/testsFront/PageHome.java
+++ src/test/java/testsFront/PageHome.java
@@ -1,5 +1,38 @@
 package testsFront;
 
-public class PageHome {
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+public class PageHome {
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", System.getenv("Driver"));
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("https://online.sp.senai.br/");
+	}
+	
+	
+	@Test
+	public void testNavegarPaginaHome() {
+		System.setProperty("webdriver.chrome.driver", System.getenv("Driver"));
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.get("https://online.sp.senai.br/");
+	}
+	
+	
+	@After
+	public void fechar() {
+		driver.close();
+	}
 }
